6ce3a3b6f1c0c79eac1911a9a5748d16a677f59e
Boyko Amarov <boyko.amarov@gmail.com>
2018-04-05 12:37:58 +0300

    Add review.

--- Problem_1.R
+++ Problem_1.R
@@ -1,3 +1,6 @@
+## WARNING: use the files Problem_1.R, Problem_2.R, etc. to write your 
+## solution otherwise your homework/exam will not be reviewed and will not be graded at all.
+
 ## Econometrics 2018
 ## Homework 1
 
@@ -11,6 +14,8 @@ y<- c(1, 4)
 # the difference between the vectors x and y 
 Difference <- x - y
 Difference
+
+## Missing explanation
 
 #1d
 s <- c(x, y)
@@ -51,12 +56,21 @@ avgMaxTemp <- mean(xmax)
 avgMaxTemp
 
 #2d
+## Using 'which' is not wrong but is unnecessary here
+## 
 xmin[which(xmin<avgMinTemp)]
 
+## Sufficient:
+xmin[xmin < avgMinTemp]
+
+
 #2e
+## Same comment as above
 xmax[which(xmax>avgMaxTemp)]
 
 #2f
+## Nice
+
 dayNames <- c('03Mon18', '04Tue18', '05Wed18', '06Thu18', '07Fri18', '08Sat18', '09Sun18')
 xmin
 str(xmin)
@@ -76,7 +90,10 @@ temperatures <- data.frame(
 
 #2h 
 xminFahrenheit <- xmin * (9/5) + 32
-temperatures <- within(temperatures, xminFahrenheit <- c(9/5*xmin + 32))
+temperatures <- within(temperatures, {
+	## Superfluous 'c'
+	xminFahrenheit <- 9/5*xmin + 32
+})
 
 #2i
 Fahrenheit <- data.frame(
@@ -94,3 +111,19 @@ dayFahrenheit <- data.frame (
 dayFahr <- data.frame(
   df.xminFahrenheit= xminFahrenheit [c(-6,-7)]
 )
+
+
+## Easier way 
+
+temperatures <- within(temperatures, {
+  xminFahrenheit <- xmin * (9/5) + 32
+  xmaxFahrenheit <- xmax * (9/5) + 32
+})
+
+temperaturesFahrenheit <- temeratures[, c('xminFahrenheit', 'xmaxFahrenheit)]
+
+## Easier to subset the whole data.frame instead of 
+## doing this for each vector used in its construction
+
+temperaturesFahrenheit[1:5, ]
+temperaturesFahrenheit[-(6:7), ]
